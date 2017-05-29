983c59c778bf57a59323c032af4699222105ac9e
Axel <axel.furlan95@gmail.com>
2017-05-28 21:04:27 -0300

    agregado schema

--- BorradoTablas.sql
+++ BorradoTablas.sql
@@ -2,80 +2,80 @@
 -- Last modification date: 2017-05-25 04:44:30.109
 
 -- foreign keys
-ALTER TABLE Auto DROP CONSTRAINT Auto_Chofer;
+ALTER TABLE SAPNU_PUAS.Auto DROP CONSTRAINT Auto_Chofer;
 
-ALTER TABLE Auto DROP CONSTRAINT Auto_Marca;
+ALTER TABLE SAPNU_PUAS.Auto DROP CONSTRAINT Auto_Marca;
 
-ALTER TABLE Auto DROP CONSTRAINT Auto_Turno;
+ALTER TABLE SAPNU_PUAS.Auto DROP CONSTRAINT Auto_Turno;
 
-ALTER TABLE Chofer DROP CONSTRAINT Chofer_Persona;
+ALTER TABLE SAPNU_PUAS.Chofer DROP CONSTRAINT Chofer_Persona;
 
-ALTER TABLE Cliente DROP CONSTRAINT Cliente_Persona;
+ALTER TABLE SAPNU_PUAS.Cliente DROP CONSTRAINT Cliente_Persona;
 
-ALTER TABLE Factura DROP CONSTRAINT Factura_Cliente;
+ALTER TABLE SAPNU_PUAS.Factura DROP CONSTRAINT Factura_Cliente;
 
-ALTER TABLE Funcionalidad_x_Rol DROP CONSTRAINT Funcionalidad_x_Rol_Funcionalidad;
+ALTER TABLE SAPNU_PUAS.Funcionalidad_x_Rol DROP CONSTRAINT Funcionalidad_x_Rol_Funcionalidad;
 
-ALTER TABLE Funcionalidad_x_Rol DROP CONSTRAINT Funcionalidad_x_Rol_Rol;
+ALTER TABLE SAPNU_PUAS.Funcionalidad_x_Rol DROP CONSTRAINT Funcionalidad_x_Rol_Rol;
 
-ALTER TABLE Persona DROP CONSTRAINT Persona_Usuario;
+ALTER TABLE SAPNU_PUAS.Persona DROP CONSTRAINT Persona_Usuario;
 
-ALTER TABLE Rendicion DROP CONSTRAINT Rendicion_Chofer;
+ALTER TABLE SAPNU_PUAS.Rendicion DROP CONSTRAINT Rendicion_Chofer;
 
-ALTER TABLE Rendicion DROP CONSTRAINT Rendicion_Turno;
+ALTER TABLE SAPNU_PUAS.Rendicion DROP CONSTRAINT Rendicion_Turno;
 
-ALTER TABLE Rol_x_Usuario DROP CONSTRAINT Rol_x_Usuario_Rol;
+ALTER TABLE SAPNU_PUAS.Rol_x_Usuario DROP CONSTRAINT Rol_x_Usuario_Rol;
 
-ALTER TABLE Rol_x_Usuario DROP CONSTRAINT Rol_x_Usuario_Usuario;
+ALTER TABLE SAPNU_PUAS.Rol_x_Usuario DROP CONSTRAINT Rol_x_Usuario_Usuario;
 
-ALTER TABLE Viaje DROP CONSTRAINT Viaje_Auto;
+ALTER TABLE SAPNU_PUAS.Viaje DROP CONSTRAINT Viaje_Auto;
 
-ALTER TABLE Viaje DROP CONSTRAINT Viaje_Chofer;
+ALTER TABLE SAPNU_PUAS.Viaje DROP CONSTRAINT Viaje_Chofer;
 
-ALTER TABLE Viaje DROP CONSTRAINT Viaje_Cliente;
+ALTER TABLE SAPNU_PUAS.Viaje DROP CONSTRAINT Viaje_Cliente;
 
-ALTER TABLE Viaje DROP CONSTRAINT Viaje_Turno;
+ALTER TABLE SAPNU_PUAS.Viaje DROP CONSTRAINT Viaje_Turno;
 
-ALTER TABLE Viaje_x_Factura DROP CONSTRAINT Viaje_x_Factura_Factura;
+ALTER TABLE SAPNU_PUAS.Viaje_x_Factura DROP CONSTRAINT Viaje_x_Factura_Factura;
 
-ALTER TABLE Viaje_x_Factura DROP CONSTRAINT Viaje_x_Factura_Viaje;
+ALTER TABLE SAPNU_PUAS.Viaje_x_Factura DROP CONSTRAINT Viaje_x_Factura_Viaje;
 
-ALTER TABLE Viaje_x_Rendicion DROP CONSTRAINT Viaje_x_Rendicion_Rendicion;
+ALTER TABLE SAPNU_PUAS.Viaje_x_Rendicion DROP CONSTRAINT Viaje_x_Rendicion_Rendicion;
 
-ALTER TABLE Viaje_x_Rendicion DROP CONSTRAINT Viaje_x_Rendicion_Viaje;
+ALTER TABLE SAPNU_PUAS.Viaje_x_Rendicion DROP CONSTRAINT Viaje_x_Rendicion_Viaje;
 
 -- tables
-DROP TABLE Auto;
+DROP TABLE SAPNU_PUAS.Auto;
 
-DROP TABLE Chofer;
+DROP TABLE SAPNU_PUAS.Chofer;
 
-DROP TABLE Cliente;
+DROP TABLE SAPNU_PUAS.Cliente;
 
-DROP TABLE Factura;
+DROP TABLE SAPNU_PUAS.Factura;
 
-DROP TABLE Funcionalidad;
+DROP TABLE SAPNU_PUAS.Funcionalidad;
 
-DROP TABLE Funcionalidad_x_Rol;
+DROP TABLE SAPNU_PUAS.Funcionalidad_x_Rol;
 
-DROP TABLE Marca;
+DROP TABLE SAPNU_PUAS.Marca;
 
-DROP TABLE Persona;
+DROP TABLE SAPNU_PUAS.Persona;
 
-DROP TABLE Rendicion;
+DROP TABLE SAPNU_PUAS.Rendicion;
 
-DROP TABLE Rol;
+DROP TABLE SAPNU_PUAS.Rol;
 
-DROP TABLE Rol_x_Usuario;
+DROP TABLE SAPNU_PUAS.Rol_x_Usuario;
 
-DROP TABLE Turno;
+DROP TABLE SAPNU_PUAS.Turno;
 
-DROP TABLE Usuario;
+DROP TABLE SAPNU_PUAS.Usuario;
 
-DROP TABLE Viaje;
+DROP TABLE SAPNU_PUAS.Viaje;
 
-DROP TABLE Viaje_x_Factura;
+DROP TABLE SAPNU_PUAS.Viaje_x_Factura;
 
-DROP TABLE Viaje_x_Rendicion;
+DROP TABLE SAPNU_PUAS.Viaje_x_Rendicion;
 
 -- End of file.
 
--- CreacionTablas.sql
+++ CreacionTablas.sql
@@ -1,9 +1,23 @@
+USE GD1C2017
+GO
+
+--DROP SCHEMA
+IF NOT EXISTS (
+	SELECT schema_name
+	FROM information_schema.schemata
+	WHERE schema_name = 'SAPNU_PUAS'
+	)
+ 
+BEGIN
+	EXEC sp_executesql N'CREATE SCHEMA SAPNU_PUAS;';
+END
+
 -- Created by Vertabelo (http://vertabelo.com)
 -- Last modification date: 2017-05-25 04:44:30.109
 
 -- tables
 -- Table: Auto
-CREATE TABLE Auto (
+CREATE TABLE SAPNU_PUAS.Auto (
     Auto_Marca int  NOT NULL,
     Auto_Modelo varchar(255)  NOT NULL,
     Auto_Patente varchar(10)  NOT NULL,
@@ -16,7 +30,7 @@ CREATE TABLE Auto (
 );
 
 -- Table: Chofer
-CREATE TABLE Chofer (
+CREATE TABLE SAPNU_PUAS.Chofer (
     Chofer_Nombre varchar(255)  NOT NULL,
     Chofer_Apellido varchar(255)  NOT NULL,
     Chofer_Dni numeric(18,0)  NOT NULL,
@@ -30,7 +44,7 @@ CREATE TABLE Chofer (
 );
 
 -- Table: Cliente
-CREATE TABLE Cliente (
+CREATE TABLE SAPNU_PUAS.Cliente (
     Cliente_Nombre varchar(255)  NOT NULL,
     Cliente_Apellido varchar(255)  NOT NULL,
     Cliente_Dni numeric(18,0)  NOT NULL,
@@ -45,7 +59,7 @@ CREATE TABLE Cliente (
 );
 
 -- Table: Factura
-CREATE TABLE Factura (
+CREATE TABLE SAPNU_PUAS.Factura (
     Factura_Nro int  NOT NULL IDENTITY(1, 1),
     Factura_Fecha_Inicio datetime  NOT NULL,
     Factura_Fecha_Fin datetime  NOT NULL,
@@ -56,28 +70,28 @@ CREATE TABLE Factura (
 );
 
 -- Table: Funcionalidad
-CREATE TABLE Funcionalidad (
+CREATE TABLE SAPNU_PUAS.Funcionalidad (
     Funcionalidad_Codigo int  NOT NULL IDENTITY(1, 1),
     Funcionalidad_Nombre varchar(255)  NOT NULL,
     CONSTRAINT Funcionalidad_pk PRIMARY KEY  (Funcionalidad_Codigo)
 );
 
 -- Table: Funcionalidad_x_Rol
-CREATE TABLE Funcionalidad_x_Rol (
+CREATE TABLE SAPNU_PUAS.Funcionalidad_x_Rol (
     Rol_Codigo int  NOT NULL,
     Funcionalidad_Codigo int  NOT NULL,
     CONSTRAINT Funcionalidad_x_Rol_pk PRIMARY KEY  (Rol_Codigo,Funcionalidad_Codigo)
 );
 
 -- Table: Marca
-CREATE TABLE Marca (
+CREATE TABLE SAPNU_PUAS.Marca (
     Marca_Codigo int  NOT NULL IDENTITY,
     Marca_Nombre varchar(255)  NOT NULL,
     CONSTRAINT Marca_pk PRIMARY KEY  (Marca_Codigo)
 );
 
 -- Table: Persona
-CREATE TABLE Persona (
+CREATE TABLE SAPNU_PUAS.Persona (
     Persona_Id int  NOT NULL IDENTITY(1, 1),
     Persona_Telefono numeric(18,0)  NOT NULL,
     Persona_Username varchar(50)  NOT NULL,
@@ -86,7 +100,7 @@ CREATE TABLE Persona (
 );
 
 -- Table: Rendicion
-CREATE TABLE Rendicion (
+CREATE TABLE SAPNU_PUAS.Rendicion (
     Rendicion_Nro numeric(18,0)  NOT NULL IDENTITY(1, 1),
     Rendicion_Fecha datetime  NOT NULL,
     Rendicion_Importe numeric(18,2)  NOT NULL,
@@ -98,7 +112,7 @@ CREATE TABLE Rendicion (
 );
 
 -- Table: Rol
-CREATE TABLE Rol (
+CREATE TABLE SAPNU_PUAS.Rol (
     Rol_Codigo int  NOT NULL IDENTITY(1, 1),
     Rol_Nombre varchar(255)  NOT NULL,
     Rol_Activo tinyint  NOT NULL,
@@ -106,14 +120,14 @@ CREATE TABLE Rol (
 );
 
 -- Table: Rol_x_Usuario
-CREATE TABLE Rol_x_Usuario (
+CREATE TABLE SAPNU_PUAS.Rol_x_Usuario (
     Rol_Codigo int  NOT NULL,
     Usuario_Username varchar(50)  NOT NULL,
     CONSTRAINT Rol_x_Usuario_pk PRIMARY KEY  (Rol_Codigo,Usuario_Username)
 );
 
 -- Table: Turno
-CREATE TABLE Turno (
+CREATE TABLE SAPNU_PUAS.Turno (
     Turno_Codigo int  NOT NULL IDENTITY(1, 1),
     Turno_Hora_Inicio numeric(18,0)  NOT NULL,
     Turno_Hora_Fin numeric(18,0)  NOT NULL,
@@ -126,7 +140,7 @@ CREATE TABLE Turno (
 );
 
 -- Table: Usuario
-CREATE TABLE Usuario (
+CREATE TABLE SAPNU_PUAS.Usuario (
     Usuario_Username varchar(50)  NOT NULL,
     Usuario_Password varchar(255)  NOT NULL,
     Usuario_Reintentos smallint  NOT NULL,
@@ -135,7 +149,7 @@ CREATE TABLE Usuario (
 );
 
 -- Table: Viaje
-CREATE TABLE Viaje (
+CREATE TABLE SAPNU_PUAS.Viaje (
     Viaje_Codigo int  NOT NULL IDENTITY(1, 1),
     Viaje_Cant_Kilometros numeric(18,0)  NOT NULL,
     Viaje_Fecha_Hora_Inicio datetime  NOT NULL,
@@ -148,14 +162,14 @@ CREATE TABLE Viaje (
 );
 
 -- Table: Viaje_x_Factura
-CREATE TABLE Viaje_x_Factura (
+CREATE TABLE SAPNU_PUAS.Viaje_x_Factura (
     Factura_Nro int  NOT NULL,
     Viaje_Codigo int  NOT NULL,
     CONSTRAINT Viaje_x_Factura_pk PRIMARY KEY  (Factura_Nro,Viaje_Codigo)
 );
 
 -- Table: Viaje_x_Rendicion
-CREATE TABLE Viaje_x_Rendicion (
+CREATE TABLE SAPNU_PUAS.Viaje_x_Rendicion (
     Viaje_Codigo int  NOT NULL,
     Rendicion_Nro numeric(18,0)  NOT NULL,
     CONSTRAINT Viaje_x_Rendicion_pk PRIMARY KEY  (Viaje_Codigo,Rendicion_Nro)
@@ -163,108 +177,108 @@ CREATE TABLE Viaje_x_Rendicion (
 
 -- foreign keys
 -- Reference: Auto_Chofer (table: Auto)
-ALTER TABLE Auto ADD CONSTRAINT Auto_Chofer
+ALTER TABLE SAPNU_PUAS.Auto ADD CONSTRAINT Auto_Chofer
     FOREIGN KEY (Auto_Chofer)
-    REFERENCES Chofer (Chofer_Telefono);
+    REFERENCES SAPNU_PUAS.Chofer (Chofer_Telefono);
 
 -- Reference: Auto_Marca (table: Auto)
-ALTER TABLE Auto ADD CONSTRAINT Auto_Marca
+ALTER TABLE SAPNU_PUAS.Auto ADD CONSTRAINT Auto_Marca
     FOREIGN KEY (Auto_Marca)
-    REFERENCES Marca (Marca_Codigo);
+    REFERENCES SAPNU_PUAS.Marca (Marca_Codigo);
 
 -- Reference: Auto_Turno (table: Auto)
-ALTER TABLE Auto ADD CONSTRAINT Auto_Turno
+ALTER TABLE SAPNU_PUAS.Auto ADD CONSTRAINT Auto_Turno
     FOREIGN KEY (Auto_Turno)
-    REFERENCES Turno (Turno_Codigo);
+    REFERENCES SAPNU_PUAS.Turno (Turno_Codigo);
 
 -- Reference: Chofer_Persona (table: Chofer)
-ALTER TABLE Chofer ADD CONSTRAINT Chofer_Persona
+ALTER TABLE SAPNU_PUAS.Chofer ADD CONSTRAINT Chofer_Persona
     FOREIGN KEY (Chofer_Persona)
-    REFERENCES Persona (Persona_Id);
+    REFERENCES SAPNU_PUAS.Persona (Persona_Id);
 
 -- Reference: Cliente_Persona (table: Cliente)
-ALTER TABLE Cliente ADD CONSTRAINT Cliente_Persona
+ALTER TABLE SAPNU_PUAS.Cliente ADD CONSTRAINT Cliente_Persona
     FOREIGN KEY (Cliente_Persona)
-    REFERENCES Persona (Persona_Id);
+    REFERENCES SAPNU_PUAS.Persona (Persona_Id);
 
 -- Reference: Factura_Cliente (table: Factura)
-ALTER TABLE Factura ADD CONSTRAINT Factura_Cliente
+ALTER TABLE SAPNU_PUAS.Factura ADD CONSTRAINT Factura_Cliente
     FOREIGN KEY (Factura_Cliente)
-    REFERENCES Cliente (Cliente_Telefono);
+    REFERENCES SAPNU_PUAS.Cliente (Cliente_Telefono);
 
 -- Reference: Funcionalidad_x_Rol_Funcionalidad (table: Funcionalidad_x_Rol)
-ALTER TABLE Funcionalidad_x_Rol ADD CONSTRAINT Funcionalidad_x_Rol_Funcionalidad
+ALTER TABLE SAPNU_PUAS.Funcionalidad_x_Rol ADD CONSTRAINT Funcionalidad_x_Rol_Funcionalidad
     FOREIGN KEY (Funcionalidad_Codigo)
-    REFERENCES Funcionalidad (Funcionalidad_Codigo);
+    REFERENCES SAPNU_PUAS.Funcionalidad (Funcionalidad_Codigo);
 
 -- Reference: Funcionalidad_x_Rol_Rol (table: Funcionalidad_x_Rol)
-ALTER TABLE Funcionalidad_x_Rol ADD CONSTRAINT Funcionalidad_x_Rol_Rol
+ALTER TABLE SAPNU_PUAS.Funcionalidad_x_Rol ADD CONSTRAINT Funcionalidad_x_Rol_Rol
     FOREIGN KEY (Rol_Codigo)
-    REFERENCES Rol (Rol_Codigo);
+    REFERENCES SAPNU_PUAS.Rol (Rol_Codigo);
 
 -- Reference: Persona_Usuario (table: Persona)
-ALTER TABLE Persona ADD CONSTRAINT Persona_Usuario
+ALTER TABLE SAPNU_PUAS.Persona ADD CONSTRAINT Persona_Usuario
     FOREIGN KEY (Persona_Username)
-    REFERENCES Usuario (Usuario_Username);
+    REFERENCES SAPNU_PUAS.Usuario (Usuario_Username);
 
 -- Reference: Rendicion_Chofer (table: Rendicion)
-ALTER TABLE Rendicion ADD CONSTRAINT Rendicion_Chofer
+ALTER TABLE SAPNU_PUAS.Rendicion ADD CONSTRAINT Rendicion_Chofer
     FOREIGN KEY (Rendicion_Chofer)
-    REFERENCES Chofer (Chofer_Telefono);
+    REFERENCES SAPNU_PUAS.Chofer (Chofer_Telefono);
 
 -- Reference: Rendicion_Turno (table: Rendicion)
-ALTER TABLE Rendicion ADD CONSTRAINT Rendicion_Turno
+ALTER TABLE SAPNU_PUAS.Rendicion ADD CONSTRAINT Rendicion_Turno
     FOREIGN KEY (Rendicion_Turno)
-    REFERENCES Turno (Turno_Codigo);
+    REFERENCES SAPNU_PUAS.Turno (Turno_Codigo);
 
 -- Reference: Rol_x_Usuario_Rol (table: Rol_x_Usuario)
-ALTER TABLE Rol_x_Usuario ADD CONSTRAINT Rol_x_Usuario_Rol
+ALTER TABLE SAPNU_PUAS.Rol_x_Usuario ADD CONSTRAINT Rol_x_Usuario_Rol
     FOREIGN KEY (Rol_Codigo)
-    REFERENCES Rol (Rol_Codigo);
+    REFERENCES SAPNU_PUAS.Rol (Rol_Codigo);
 
 -- Reference: Rol_x_Usuario_Usuario (table: Rol_x_Usuario)
-ALTER TABLE Rol_x_Usuario ADD CONSTRAINT Rol_x_Usuario_Usuario
+ALTER TABLE SAPNU_PUAS.Rol_x_Usuario ADD CONSTRAINT Rol_x_Usuario_Usuario
     FOREIGN KEY (Usuario_Username)
-    REFERENCES Usuario (Usuario_Username);
+    REFERENCES SAPNU_PUAS.Usuario (Usuario_Username);
 
 -- Reference: Viaje_Auto (table: Viaje)
-ALTER TABLE Viaje ADD CONSTRAINT Viaje_Auto
+ALTER TABLE SAPNU_PUAS.Viaje ADD CONSTRAINT Viaje_Auto
     FOREIGN KEY (Viaje_Auto)
-    REFERENCES Auto (Auto_Patente);
+    REFERENCES SAPNU_PUAS.Auto (Auto_Patente);
 
 -- Reference: Viaje_Chofer (table: Viaje)
-ALTER TABLE Viaje ADD CONSTRAINT Viaje_Chofer
+ALTER TABLE SAPNU_PUAS.Viaje ADD CONSTRAINT Viaje_Chofer
     FOREIGN KEY (Viaje_Chofer)
-    REFERENCES Chofer (Chofer_Telefono);
+    REFERENCES SAPNU_PUAS.Chofer (Chofer_Telefono);
 
 -- Reference: Viaje_Cliente (table: Viaje)
-ALTER TABLE Viaje ADD CONSTRAINT Viaje_Cliente
+ALTER TABLE SAPNU_PUAS.Viaje ADD CONSTRAINT Viaje_Cliente
     FOREIGN KEY (Viaje_Cliente)
-    REFERENCES Cliente (Cliente_Telefono);
+    REFERENCES SAPNU_PUAS.Cliente (Cliente_Telefono);
 
 -- Reference: Viaje_Turno (table: Viaje)
-ALTER TABLE Viaje ADD CONSTRAINT Viaje_Turno
+ALTER TABLE SAPNU_PUAS.Viaje ADD CONSTRAINT Viaje_Turno
     FOREIGN KEY (Viaje_Turno)
-    REFERENCES Turno (Turno_Codigo);
+    REFERENCES SAPNU_PUAS.Turno (Turno_Codigo);
 
 -- Reference: Viaje_x_Factura_Factura (table: Viaje_x_Factura)
-ALTER TABLE Viaje_x_Factura ADD CONSTRAINT Viaje_x_Factura_Factura
+ALTER TABLE SAPNU_PUAS.Viaje_x_Factura ADD CONSTRAINT Viaje_x_Factura_Factura
     FOREIGN KEY (Factura_Nro)
-    REFERENCES Factura (Factura_Nro);
+    REFERENCES SAPNU_PUAS.Factura (Factura_Nro);
 
 -- Reference: Viaje_x_Factura_Viaje (table: Viaje_x_Factura)
-ALTER TABLE Viaje_x_Factura ADD CONSTRAINT Viaje_x_Factura_Viaje
+ALTER TABLE SAPNU_PUAS.Viaje_x_Factura ADD CONSTRAINT Viaje_x_Factura_Viaje
     FOREIGN KEY (Viaje_Codigo)
-    REFERENCES Viaje (Viaje_Codigo);
+    REFERENCES SAPNU_PUAS.Viaje (Viaje_Codigo);
 
 -- Reference: Viaje_x_Rendicion_Rendicion (table: Viaje_x_Rendicion)
-ALTER TABLE Viaje_x_Rendicion ADD CONSTRAINT Viaje_x_Rendicion_Rendicion
+ALTER TABLE SAPNU_PUAS.Viaje_x_Rendicion ADD CONSTRAINT Viaje_x_Rendicion_Rendicion
     FOREIGN KEY (Rendicion_Nro)
-    REFERENCES Rendicion (Rendicion_Nro);
+    REFERENCES SAPNU_PUAS.Rendicion (Rendicion_Nro);
 
 -- Reference: Viaje_x_Rendicion_Viaje (table: Viaje_x_Rendicion)
-ALTER TABLE Viaje_x_Rendicion ADD CONSTRAINT Viaje_x_Rendicion_Viaje
+ALTER TABLE SAPNU_PUAS.Viaje_x_Rendicion ADD CONSTRAINT Viaje_x_Rendicion_Viaje
     FOREIGN KEY (Viaje_Codigo)
-    REFERENCES Viaje (Viaje_Codigo);
+    REFERENCES SAPNU_PUAS.Viaje (Viaje_Codigo);
 
 -- End of file.
