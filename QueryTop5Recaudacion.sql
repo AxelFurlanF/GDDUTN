316e5ae91ea251251253781ab2f7fe77a8ac61a3
Axel <axel.furlan95@gmail.com>
2017-05-31 23:44:24 -0300

    añadido top 5 recaudacion

--- QueryTop5Recaudacion.sql
+++ QueryTop5Recaudacion.sql
@@ -2,7 +2,7 @@ CREATE FUNCTION SAPNU_PUAS.top5recaudacion (@fechaInicio datetime, @fechaFin dat
 RETURNS TABLE
 AS
 RETURN
-	SELECT TOP 5 Chofer_Telefono, SUM(Rendicion_Importe) 
+	SELECT TOP 5 SUM(Rendicion_Importe) as RecaudacionTotal, Chofer_Apellido, Chofer_Telefono, Chofer_Mail
 	FROM SAPNU_PUAS.Chofer JOIN SAPNU_PUAS.Rendicion ON Rendicion_Chofer=Chofer_Telefono
 	WHERE Rendicion_Fecha BETWEEN @fechaInicio AND @fechaFin
 	GROUP BY Chofer_Telefono
