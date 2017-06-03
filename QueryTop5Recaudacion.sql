5d26e99e8c8432d7f169710109b1b70e3034797e
Axel <axel.furlan95@gmail.com>
2017-06-03 14:37:52 -0300

    añadido top 5 recaudacion con anio, mes1 y mes 2

--- QueryTop5Recaudacion.sql
+++ QueryTop5Recaudacion.sql
@@ -1,9 +1,9 @@
-CREATE FUNCTION SAPNU_PUAS.top5recaudacion (@fechaInicio datetime, @fechaFin datetime)
+CREATE FUNCTION SAPNU_PUAS.top5recaudacion (@anio int, @mes1 int, @mes2 int)
 RETURNS TABLE
 AS
 RETURN
 	SELECT TOP 5 SUM(Rendicion_Importe) as RecaudacionTotal, Chofer_Apellido, Chofer_Telefono, Chofer_Mail
 	FROM SAPNU_PUAS.Chofer JOIN SAPNU_PUAS.Rendicion ON Rendicion_Chofer=Chofer_Telefono
-	WHERE Rendicion_Fecha BETWEEN @fechaInicio AND @fechaFin
+	WHERE YEAR(Rendicion_Fecha)=@anio AND MONTH(Rendicion_Fecha) BETWEEN @mes1 and @mes2
 	GROUP BY Chofer_Telefono
 	ORDER BY SUM (Rendicion_Importe) DESC;
