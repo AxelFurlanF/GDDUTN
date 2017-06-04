13bfe9d1591600b9f597b7d0efd093e8f0a1a8be
Axel <axel.furlan95@gmail.com>
2017-06-03 21:29:36 -0300

    corregida agrupacion

--- QueryTop5Recaudacion.sql
+++ QueryTop5Recaudacion.sql
@@ -5,5 +5,5 @@ RETURN
 	SELECT TOP 5 SUM(Rendicion_Importe) as RecaudacionTotal, Chofer_Apellido, Chofer_Telefono, Chofer_Mail
 	FROM SAPNU_PUAS.Chofer JOIN SAPNU_PUAS.Rendicion ON Rendicion_Chofer=Chofer_Telefono
 	WHERE YEAR(Rendicion_Fecha)=@anio AND MONTH(Rendicion_Fecha) BETWEEN @mes1 and @mes2
-	GROUP BY Chofer_Telefono
+	GROUP BY Chofer_Apellido, Chofer_Telefono, Chofer_Mail
 	ORDER BY SUM (Rendicion_Importe) DESC;
